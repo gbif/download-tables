71bb3c9ad763b20e927113130fc9559a16375e9d
Dave Martin <djtfmartin@gmail.com>
2026-09-03 20:06:29 +0100

    feature(pipelines): work for nested struct occurrence#543

--- hdfs-tables/src/main/java/org/gbif/occurrence/download/hive/HiveDataTypes.java
+++ hdfs-tables/src/main/java/org/gbif/occurrence/download/hive/HiveDataTypes.java
@@ -272,6 +272,8 @@ public final class HiveDataTypes {
       return TYPE_MAP_OF_ARRAY_STRUCT;
     } else if (term.equals(GbifInternalTerm.taxonomicStatuses)) {
       return TYPE_MAP_STRUCT;
+    } else if (term.equals(GbifInternalTerm.taxonomicIssues)) {
+      return TYPE_MAP_OF_ARRAY_STRUCT;
     } else if (term.equals(GbifTerm.nonTaxonomicIssue)) {
       return TYPE_ARRAY_STRING;
     } else if (term.equals(GbifTerm.taxonomicIssue)) {
